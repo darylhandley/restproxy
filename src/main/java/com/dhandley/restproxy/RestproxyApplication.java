5829db23adf0c1fee0dc775c1b0f8ee4ba31dd0b
dhandley <dhandley@sonatype.com>
2021-08-23 14:36:49 -0700

    Initial commit

---
+++ src/main/java/com/dhandley/restproxy/RestproxyApplication.java
@@ -0,0 +1,13 @@
+package com.dhandley.restproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestproxyApplication.class, args);
+	}
+
+}
